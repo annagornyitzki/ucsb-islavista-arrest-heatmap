4382f44132352e839cf6841e8418ff7521f6ef64
Anna Gornyitzki <146987923+annagornyitzki@users.noreply.github.com>
2024-12-29 15:29:03 -0800

    Update arrestQueries.sql

--- Database/arrestQueries.sql
+++ Database/arrestQueries.sql
@@ -1,6 +1,7 @@
-
+-- Display Table
 SELECT * FROM UCSB_Crime_Fire_Log ucfl;
 
+-- 
 SELECT COUNT(*) AS cc, Classification FROM UCSB_Crime_Fire_Log ucfl WHERE Disposition LIKE 'Arrest%' GROUP BY Classification;
 
-
+SELECT Location || ", Isla Vista, CA" as address FROM UCSB_Crime_Fire_Log ucfl WHERE Disposition LIKE "Arrest%"
